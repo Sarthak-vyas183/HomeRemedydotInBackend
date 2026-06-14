b787832ec610690ea2525c49b1362b41f380bb57
Sarthak Vyas <vyassarthak183@gmail.com>
2026-06-14 12:16:44 +0530

    update code

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import connectDB from "./db/dbconnect.js";
 import {app} from './app.js'
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const projectRoot = path.resolve(__dirname, "..");
 
 dotenv.config({
-    path: './.env'
+    path: path.resolve(projectRoot, "..", ".env")
 })
 
-const __dirname = path.resolve();
 app.use("/public", express.static(path.join(__dirname, "public")));
 
 connectDB()
